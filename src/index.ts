971ab8a4194b575ee2e8679033721f68a1590122
Domaeos <ryanmurphy.ic@icloud.com>
2023-12-24 15:55:21 +0000

    env and authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import authRouter from "./routes/authRouter";
+import connectUserDB from "./connections/userDB";
+import dotenv from "dotenv";
+
+dotenv.config();
+connectUserDB();
 
 const app = express();
+app.use(express.json())
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+app.use()
 app.use(authRouter);
